e942a5c585091e443d62a2aa4f42e45ee4679c27
DESKTOP-RCFES7K\Admin <tinlkp.lrmt212@gmail.com>
2023-10-22 09:15:41 +0700

    admin comment

--- src/services/AdminLocation.js
+++ src/services/AdminLocation.js
@@ -60,6 +60,19 @@ class AdminLocationService {
       data,
     });
   }
+  fetchAdminImgLocationApi(id, data) {
+    return axios({
+      url: `https://airbnbnew.cybersoft.edu.vn/api/vi-tri/upload-hinh-vitri?maViTri=${id}`,
+      method: "POST",
+      headers: {
+        tokenCybersoft:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MSIsIkhldEhhblN0cmluZyI6IjA4LzAzLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwOTg1NjAwMDAwMCIsIm5iZiI6MTY4MDM2ODQwMCwiZXhwIjoxNzEwMDAzNjAwfQ.SWV-oXLgyWKt3LK_5cUNjLzDMJ3vka3jrLF73mhH7I8",
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM4ODMiLCJlbWFpbCI6Inp4emN4ekBnbWFpbC5jb20iLCJyb2xlIjoiQURNSU4iLCJuYmYiOjE2OTc1OTM4OTQsImV4cCI6MTY5ODE5ODY5NH0.dq9xdj71fPQcBgsgFuG9Y8MP_p4uoND0b-mJbTl4SLo",
+      },
+      data,
+    });
+  }
 }
 
 export const adminLocalService = new AdminLocationService();
