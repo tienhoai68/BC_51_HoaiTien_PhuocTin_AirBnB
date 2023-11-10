4438ee869bbcd7937f207caa3b04f81de75ef509
DESKTOP-RCFES7K\Admin <tinlkp.lrmt212@gmail.com>
2023-11-10 07:04:39 +0700

    add valication for adminLocal and fix bug adminRoom

--- src/pages/AdminAddLocation/AdminAddLocation.jsx
+++ src/pages/AdminAddLocation/AdminAddLocation.jsx
@@ -1,10 +1,13 @@
-import { Form } from "antd";
 import React, { useState } from "react";
 import Swal from "sweetalert2";
 import { adminLocalService } from "../../services/AdminLocation";
 import { useNavigate } from "react-router-dom";
+import { createRef } from "react";
 
 export default function AdminAddLocation() {
+  const tenViTriRef = createRef();
+  const tinhThanhRef = createRef();
+  const quocGiaRef = createRef();
   const navigate = useNavigate();
   const [addLocation, setAddLocation] = useState({
     tenViTri: "",
@@ -19,25 +22,53 @@ export default function AdminAddLocation() {
       [event.target.name]: event.target.value,
     });
   };
-  const handleSubmit = async () => {
-    try {
-      const result = await adminLocalService.fetchAdminAddLocationApi(
-        addLocation
-      );
-      Swal.fire({
-        icon: "success",
-        title: "Success!",
-        text: "Thêm vị trí thành công !",
-      });
-      if (result.data.content) {
-        navigate(`/admin/location`);
+  const validateRequired = (value, ref, mes) => {
+    if (value !== "") {
+      ref.innerHTML = "";
+      return true;
+    }
+    ref.innerHTML = mes;
+    return false;
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    let isValid = true;
+    isValid &= validateRequired(
+      addLocation.tenViTri,
+      tenViTriRef.current,
+      "Tên vị trí là bắt buộc!!!"
+    );
+    isValid &= validateRequired(
+      addLocation.tinhThanh,
+      tinhThanhRef.current,
+      "Tên tỉnh thành là bắt buộc!!!"
+    );
+    isValid &= validateRequired(
+      addLocation.quocGia,
+      quocGiaRef.current,
+      "Tên quốc gia là bắt buộc!!!"
+    );
+    if (isValid) {
+      try {
+        const result = await adminLocalService.fetchAdminAddLocationApi(
+          addLocation
+        );
+        Swal.fire({
+          icon: "success",
+          title: "Success!",
+          text: "Thêm vị trí thành công !",
+        });
+        if (result.data.content) {
+          navigate(`/admin/location`);
+        }
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: `${error.response.data.content}`,
+        });
       }
-    } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: `${error.response.data.content}`,
-      });
     }
   };
 
@@ -46,51 +77,58 @@ export default function AdminAddLocation() {
       <div className="md:block text-center text-3xl text-blue-800">
         <h1>Thêm Vị Trí</h1>
       </div>
-      <Form
-        onSubmitCapture={handleSubmit}
-        labelCol={{
-          span: 4,
-        }}
-        wrapperCol={{
-          span: 14,
-        }}
-        style={{
-          maxWidth: 1000,
-        }}
-      >
-        <Form.Item label="Tên Vị Trí: ">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Tên Vị trí :
+          </label>
           <input
             type="text"
             onChange={handleChange}
             name="tenViTri"
-            className="border text-sm rounded-md w-2/3 p-2 "
+            className="border text-sm rounded-md w-1/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item label="Tỉnh Thành: ">
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={tenViTriRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Tên Tỉnh Thành :
+          </label>
           <input
             type="text"
             onChange={handleChange}
             name="tinhThanh"
-            className="border text-sm rounded-md w-2/3 p-2 "
+            className="border text-sm rounded-md w-1/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item label="Quốc Gia: ">
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={tinhThanhRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Tên Quốc Gia:
+          </label>
           <input
             type="text"
             onChange={handleChange}
             name="quocGia"
-            className="border text-sm rounded-md w-2/3 p-2 "
+            className="border text-sm rounded-md w-1/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item label="Action">
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={quocGiaRef}></span>
+        </div>
+        <div className="mb-2">
           <button
             type="submit"
             className=" font-medium text-sm py-2.5 mr-2 mb-2 bg-blue-500 p-3 rounded-md"
           >
             Thêm
           </button>
-        </Form.Item>
-      </Form>
+        </div>
+      </form>
     </div>
   );
 }
--- src/pages/AdminAddRoom/AdminAddRoom.jsx
+++ src/pages/AdminAddRoom/AdminAddRoom.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
-import { Form, Switch } from "antd";
+import React, { createRef, useState } from "react";
+import { Switch } from "antd";
 import "./AdminAddRoom.scss";
 import { adminRoomService } from "../../services/AdminRoom";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 
 export default function AdminAddRoom() {
+  const tenPhongInputRef = createRef();
+  const khachRef = createRef();
+  const phongNguRef = createRef();
+  const giuongRef = createRef();
+  const phongTamRef = createRef();
+  const giaTienRef = createRef();
+  const maViTriRef = createRef();
   const navigate = useNavigate();
-  const [error, setError] = useState(false);
+
   const [roomState, setRoomState] = useState({
     tenPhong: "",
-    khach: 0,
-    phongNgu: 0,
-    giuong: 0,
-    phongTam: 0,
+    khach: "",
+    phongNgu: "",
+    giuong: "",
+    phongTam: "",
     moTa: "",
-    giaTien: 0,
+    giaTien: "",
     mayGiat: false,
     banLa: false,
     tivi: false,
@@ -25,10 +32,26 @@ export default function AdminAddRoom() {
     doXe: false,
     hoBoi: false,
     banUi: false,
-    maViTri: 0,
+    maViTri: "",
     hinhAnh: "",
   });
+  const validateRequired = (value, ref, mes) => {
+    if (value !== "") {
+      ref.innerHTML = "";
+      return true;
+    }
+    ref.innerHTML = mes;
+    return false;
+  };
 
+  const validateCheck = (value, ref, mes, letter) => {
+    if (letter.test(value)) {
+      ref.innerHTML = "";
+      return true;
+    }
+    ref.innerHTML = mes;
+    return false;
+  };
   const handleChange = (event) => {
     setRoomState({
       ...roomState,
@@ -41,8 +64,59 @@ export default function AdminAddRoom() {
       [event.target.offsetParent.name]: value,
     });
   };
-  const handleSubmit = async () => {
-    if (error) {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let isValid = true;
+    isValid &= validateRequired(
+      roomState.tenPhong,
+      tenPhongInputRef.current,
+      "Không để trống tên phòng"
+    );
+    isValid &=
+      validateRequired(
+        roomState.khach,
+        khachRef.current,
+        "Không để trống số khách"
+      ) &&
+      validateCheck(
+        roomState.khach,
+        khachRef.current,
+        "số khách phải là số",
+        /^[0-9]+$/
+      );
+    isValid &= validateRequired(
+      roomState.phongNgu,
+      phongNguRef.current,
+      "Không để trống số phòng ngủ"
+    );
+    isValid &= validateRequired(
+      roomState.phongTam,
+      phongTamRef.current,
+      "Không để trống số phòng tắm"
+    );
+    isValid &= validateRequired(
+      roomState.giuong,
+      giuongRef.current,
+      "Không để trống số giường"
+    );
+    isValid &=
+      validateRequired(
+        roomState.giaTien,
+        giaTienRef.current,
+        "Không để trống"
+      ) &&
+      validateCheck(
+        roomState.giaTien,
+        giaTienRef.current,
+        "Giá tiền phải là số",
+        /^[0-9]+$/
+      );
+    isValid &= validateRequired(
+      roomState.maViTri,
+      maViTriRef.current,
+      "Không để trống mã vị trí"
+    );
+    if (isValid) {
       try {
         const result = await adminRoomService.fetchAdminAddRoomApi(roomState);
         Swal.fire({
@@ -62,230 +136,218 @@ export default function AdminAddRoom() {
       }
     }
   };
-  const onFinish = (values) => {
-    if (values) {
-      setError(true);
-    }
-  };
-  const onFinishFailed = (errorInfo) => {
-    if (errorInfo) {
-      setError(false);
-    }
-  };
   return (
-    <div className="container mx-auto py-5">
+    <div className="container mx-auto py-5 ">
       <div className="md:block text-center text-3xl text-blue-800">
         <h1>Thêm Phòng Thuê</h1>
       </div>
-      <Form
-        labelCol={{
-          span: 4,
-        }}
-        wrapperCol={{
-          span: 14,
-        }}
-        style={{
-          maxWidth: 1000,
-        }}
-        autoComplete="off"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        onSubmitCapture={handleSubmit}
-      >
-        <Form.Item
-          label="Tên Phòng: "
-          name="tenPhong"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Tên Phòng :
+          </label>
           <input
             onChange={handleChange}
             type="text"
             name="tenPhong"
-            className="border text-sm rounded-md w-2/3 p-2 "
+            className="border text-sm rounded-md w-1/3 p-2 mb-2"
           />
-        </Form.Item>
-        <Form.Item
-          label="Số Khách :"
-          name="khach"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={tenPhongInputRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Số khách :
+          </label>
           <input
-            min={1}
             onChange={handleChange}
+            min={1}
             type="number"
             name="khach"
-            className="border text-sm rounded-md w-1/3 p-2 "
+            className="border text-sm rounded-md w-0.5/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item
-          label="Phòng Ngủ :"
-          name="phongNgu"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={khachRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Phòng ngủ :
+          </label>
           <input
-            min={1}
             onChange={handleChange}
+            min={1}
             type="number"
             name="phongNgu"
-            className="border text-sm rounded-md w-1/3 p-2 "
+            className="border text-sm rounded-md  w-0.5/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item
-          label="Số Giường :"
-          name="giuong"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={phongNguRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Giường :
+          </label>
           <input
-            min={1}
             onChange={handleChange}
+            min={1}
             type="number"
             name="giuong"
-            className="border text-sm rounded-md w-1/3 p-2 "
+            className="border text-sm rounded-md  w-0.5/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item
-          label="Số Phòng Tắm :"
-          name="phongTam"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={giuongRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Phòng Tắm :
+          </label>
           <input
-            min={1}
             onChange={handleChange}
+            min={1}
             type="number"
             name="phongTam"
-            className="border text-sm rounded-md w-1/3 p-2 "
+            className="border text-sm rounded-md  w-0.5/3 p-2 "
           />
-        </Form.Item>
-
-        <Form.Item
-          label=" Mã Vị Trí :"
-          name="maViTri"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={phongTamRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Mã vị trí :
+          </label>
           <input
-            max={10}
-            min={0}
             onChange={handleChange}
+            min={0}
             type="number"
             name="maViTri"
-            className="border text-sm rounded-md w-1/3 p-2 "
+            className="border text-sm rounded-md  w-0.5/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item
-          label="Mô Tả: "
-          name="moTa"
-          rules={[
-            {
-              required: true,
-              message: "Không để trống!",
-            },
-          ]}
-        >
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={maViTriRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">Mô tả :</label>
           <input
             onChange={handleChange}
             type="text"
             name="moTa"
-            className="border text-sm rounded-md w-2/3 p-2 "
+            className="border text-sm rounded-md  w-0.5/3 p-2 "
           />
-        </Form.Item>
-        <Form.Item label="Máy Giặt:" valuePropName="checked">
-          <Switch name="mayGiat" className="adminSwitch" />
-        </Form.Item>
-        <Form.Item label="Bàn Là:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Giá tiền :
+          </label>
+          <input
+            onChange={handleChange}
+            min={1}
+            type="number"
+            name="giaTien"
+            className="border text-sm rounded-md  w-0.5/3 p-2 "
+          />
+        </div>
+        <div style={{ color: "red" }}>
+          <span className="text-danger" ref={giaTienRef}></span>
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Máy giặt :
+          </label>
+          <Switch
+            onClick={handleChangeSwitch}
+            name="mayGiat"
+            className="adminSwitch"
+          />
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Bàn là :
+          </label>
           <Switch
             onClick={handleChangeSwitch}
             name="banLa"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Tivi:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">Tivi :</label>
           <Switch
             onClick={handleChangeSwitch}
             name="tivi"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Điều Hòa:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Điều hòa :
+          </label>
           <Switch
             onClick={handleChangeSwitch}
             name="dieuHoa"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Wifi:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">Wifi :</label>
           <Switch
             onClick={handleChangeSwitch}
             name="wifi"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Bếp:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">Bếp :</label>
           <Switch
             onClick={handleChangeSwitch}
             name="bep"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Bãi Đỗ Xe:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Bãi đõ xe :
+          </label>
           <Switch
             onClick={handleChangeSwitch}
             name="doXe"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Hồ Bơi:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Hồ bơi :
+          </label>
           <Switch
             onClick={handleChangeSwitch}
             name="hoBoi"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Bàn Ủi:" valuePropName="checked">
+        </div>
+        <div className="mb-2">
+          <label className="block mb-2 text-sm font-medium mr-3">
+            Bàn Ủi :
+          </label>
           <Switch
             onClick={handleChangeSwitch}
             name="banUi"
             className="adminSwitch"
           />
-        </Form.Item>
-        <Form.Item label="Action">
+        </div>
+        <div className="mb-2">
           <button
             type="submit"
-            className=" font-medium text-sm py-2.5 mr-2 mb-2 bg-blue-500 p-3 rounded-md"
+            className=" font-medium text-sm py-2.5 m-3 bg-blue-500 p-5 rounded-md"
           >
             Thêm
           </button>
-        </Form.Item>
-      </Form>
+        </div>
+      </form>
     </div>
   );
 }
