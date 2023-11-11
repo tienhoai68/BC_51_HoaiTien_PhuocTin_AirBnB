fd1a6c6c9ccef0acb9c6877533b8d424794d4496
DESKTOP-RCFES7K\Admin <tinlkp.lrmt212@gmail.com>
2023-11-11 10:33:38 +0700

    validate for addRoom and addLocation

--- src/pages/AdminAddLocation/AdminAddLocation.jsx
+++ src/pages/AdminAddLocation/AdminAddLocation.jsx
@@ -2,73 +2,41 @@ import React, { useState } from "react";
 import Swal from "sweetalert2";
 import { adminLocalService } from "../../services/AdminLocation";
 import { useNavigate } from "react-router-dom";
-import { createRef } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  tenViTri: Yup.string().required("(*) Không để trống tên Vị Trí"),
+  tinhThanh: Yup.string().required("(*) Không để trống tên Tỉnh Thành"),
+  quocGia: Yup.string().required("(*) Không để trống tên Quốc Gia"),
+});
 
 export default function AdminAddLocation() {
-  const tenViTriRef = createRef();
-  const tinhThanhRef = createRef();
-  const quocGiaRef = createRef();
   const navigate = useNavigate();
-  const [addLocation, setAddLocation] = useState({
+  const initialValues = {
     tenViTri: "",
     tinhThanh: "",
     quocGia: "",
     hinhAnh: "",
-  });
-
-  const handleChange = (event) => {
-    setAddLocation({
-      ...addLocation,
-      [event.target.name]: event.target.value,
-    });
   };
-  const validateRequired = (value, ref, mes) => {
-    if (value !== "") {
-      ref.innerHTML = "";
-      return true;
-    }
-    ref.innerHTML = mes;
-    return false;
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    let isValid = true;
-    isValid &= validateRequired(
-      addLocation.tenViTri,
-      tenViTriRef.current,
-      "Tên vị trí là bắt buộc!!!"
-    );
-    isValid &= validateRequired(
-      addLocation.tinhThanh,
-      tinhThanhRef.current,
-      "Tên tỉnh thành là bắt buộc!!!"
-    );
-    isValid &= validateRequired(
-      addLocation.quocGia,
-      quocGiaRef.current,
-      "Tên quốc gia là bắt buộc!!!"
-    );
-    if (isValid) {
-      try {
-        const result = await adminLocalService.fetchAdminAddLocationApi(
-          addLocation
-        );
-        Swal.fire({
-          icon: "success",
-          title: "Success!",
-          text: "Thêm vị trí thành công !",
-        });
-        if (result.data.content) {
-          navigate(`/admin/location`);
-        }
-      } catch (error) {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: `${error.response.data.content}`,
-        });
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      const result = await adminLocalService.fetchAdminAddLocationApi(values);
+      Swal.fire({
+        icon: "success",
+        title: "Success!",
+        text: "Thêm vị trí thành công !",
+      });
+      resetForm();
+      if (result.data.content) {
+        navigate(`/admin/location`);
       }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `${error.response.data.content}`,
+      });
     }
   };
 
@@ -77,58 +45,70 @@ export default function AdminAddLocation() {
       <div className="md:block text-center text-3xl text-blue-800">
         <h1>Thêm Vị Trí</h1>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-2">
-          <label className="block mb-2 text-sm font-medium mr-3">
-            Tên Vị trí :
-          </label>
-          <input
-            type="text"
-            onChange={handleChange}
-            name="tenViTri"
-            className="border text-sm rounded-md w-1/3 p-2 "
-          />
-        </div>
-        <div style={{ color: "red" }}>
-          <span className="text-danger" ref={tenViTriRef}></span>
-        </div>
-        <div className="mb-2">
-          <label className="block mb-2 text-sm font-medium mr-3">
-            Tên Tỉnh Thành :
-          </label>
-          <input
-            type="text"
-            onChange={handleChange}
-            name="tinhThanh"
-            className="border text-sm rounded-md w-1/3 p-2 "
-          />
-        </div>
-        <div style={{ color: "red" }}>
-          <span className="text-danger" ref={tinhThanhRef}></span>
-        </div>
-        <div className="mb-2">
-          <label className="block mb-2 text-sm font-medium mr-3">
-            Tên Quốc Gia:
-          </label>
-          <input
-            type="text"
-            onChange={handleChange}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="mb-2">
+            <label className="block mb-2 text-sm font-medium mr-3">
+              Tên Vị trí :
+            </label>
+            <Field
+              type="text"
+              id="tenViTri"
+              name="tenViTri"
+              className="border text-sm rounded-md w-1/3 p-2 "
+            />
+            <ErrorMessage
+              name="tenViTri"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2 text-sm font-medium mr-3">
+              Tên Tỉnh Thành :
+            </label>
+            <Field
+              type="text"
+              id="tinhThanh"
+              name="tinhThanh"
+              className="border text-sm rounded-md w-1/3 p-2 "
+            />
+            <ErrorMessage
+              name="tinhThanh"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2 text-sm font-medium mr-3">
+              Tên Quốc Gia:
+            </label>
+            <Field
+              type="text"
+              id="quocGia"
+              name="quocGia"
+              className="border text-sm rounded-md w-1/3 p-2 "
+            />
+          </div>
+          <ErrorMessage
             name="quocGia"
-            className="border text-sm rounded-md w-1/3 p-2 "
+            component="div"
+            className="text-red-500"
           />
-        </div>
-        <div style={{ color: "red" }}>
-          <span className="text-danger" ref={quocGiaRef}></span>
-        </div>
-        <div className="mb-2">
-          <button
-            type="submit"
-            className=" font-medium text-sm py-2.5 mr-2 mb-2 bg-blue-500 p-3 rounded-md"
-          >
-            Thêm
-          </button>
-        </div>
-      </form>
+          <div className="mb-2">
+            <button
+              type="submit"
+              className=" font-medium text-sm py-2.5 mr-2 mb-2 bg-blue-500 p-3 rounded-md"
+            >
+              Thêm
+            </button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 }
